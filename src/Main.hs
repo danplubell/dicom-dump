ff86c299d9bf1aff6860d920c28fba070cfeaae2
Dan Plubell <dplubell@cerner.com>
2016-01-11 14:12:42 -0600

    add partial display of data, switch functions to parse dicom file

--- src/Main.hs
+++ src/Main.hs
@@ -3,26 +3,24 @@
 module Main where
 
 import qualified Data.ByteString.Char8 as BSC
-import qualified Data.ByteString.Lazy  as BL
 import           Data.DICOM.Dictionary
 import           Data.DICOM.Model
 import           Data.DICOM.Parse
 import           System.Environment
-import           System.IO
 import           Text.Printf
 main :: IO ()
 main = do
   (fp:_) <- getArgs
-  h <- openBinaryFile fp ReadMode
-  contents <- BL.hGetContents h
   let dd = loadElementDictionary
-  let fc = parseDicomFileContent contents
-  mapM_ (pPrintf dd) fc
+  fc  <- parseDicomFile fp
+  case fc of
+    Left s -> putStrLn $ "A parsing error occured: " ++ s
+    Right elements -> mapM_ (pPrintf dd) elements
 
 pPrintf::PrintfType t =>DicomDictionary -> DataElement -> t
 pPrintf dd de =
   case de of
-    Element {..} -> printf "(%04x,%04x) %s % 10d %s\n" (fst deTag ) (snd deTag ) (show deVR ) deVL (BSC.unpack $ lookupEleName deTag)
-    Item {..}    -> printf "(%04x,%04x) na % 10d %s\n" (fst diTag ) (snd diTag ) diVL (BSC.unpack $ lookupEleName diTag)
+    Element {..} -> printf "(%04x,%04x) %s % 10d %- 40s %- 20s\n" (fst deTag ) (snd deTag ) (show deVR ) deVL (BSC.unpack $ lookupEleName deTag) ((show . BSC.take 40) deRawValue)
+    Item {..}    -> printf "(%04x,%04x) na % 10d %- 40s\n" (fst diTag ) (snd diTag ) diVL (BSC.unpack $ lookupEleName diTag)
   where lookupEleName t = maybe "Unknown Tag and Data" name (lookupElementByTag dd t)
 
